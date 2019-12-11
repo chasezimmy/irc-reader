beeb1034d2dee1f324b1b084060fc42ddec30b75
Chase Zimmerman <chase.zimmerman@modaoperandi.com>
2019-12-11 17:28:02 -0500

    Adding Redis and other stuff (#4)
    * adding redis fun stuff
    
    * celery group
    
    * redis cache
    
    * fix task

--- app/__init__.py
+++ app/__init__.py
@@ -1,30 +1,31 @@
-import threading
-from flask import Flask, current_app
+from flask import Flask
 from celery import Celery
+from celery.utils.log import get_task_logger
 from config import Config
 from redis_client import redis_client
-from irc.irc import irc_routes
+from irc.routes import irc
+from data.routes import data
 from irc.irc_listener import IRCListener
-from data import data_routes
+
 
 celery = Celery(__name__, broker=Config.CELERY_BROKER_URL)
-threads = {}
+logger = get_task_logger(__name__)
 
 def create_app():
 
     app = Flask(__name__)
     app.config['JSON_AS_ASCII'] = False
     #app.config['DEBUG'] = True
-    app.register_blueprint(irc_routes)
-    app.register_blueprint(data_routes)
+    app.register_blueprint(irc)
+    app.register_blueprint(data)
     app.config.from_object(Config)
     celery.conf.update(app.config)
     
     return app
 
 
-@celery.task(name='join')
-def join(channel):
+@celery.task(name='join_channel')
+def join_channel(channel):
     redis_client.hset('channels', channel, 1)
-    IRCListener(channel, redis_client)
+    IRCListener(channel, redis_client, logger)
     return f'PARTING >> {channel}'
--- app/scheduled_tasks.py
+++ app/scheduled_tasks.py
@@ -1,8 +1,8 @@
-import json
 import time
+import json
 from celery import group
 from redis_client import redis_client
-from . import join
+from app import join_channel
 from .top_channels import top_channels
 
 
@@ -15,12 +15,13 @@ def refresh_top_channels():
             current_threads.add(k.decode())
 
     top = top_channels()
-
+    
     to_remove = current_threads.difference(top)
     to_add = top.difference(current_threads)
 
+    print('curr: ', redis_client.hgetall('channels').items())
     print('add: ', to_add)
-    group([join.s(channel) for channel in to_add]).apply_async()
+    group([join_channel.s(channel) for channel in to_add]).apply_async()
 
     print('remove: ', to_remove)
     for channel in to_remove:
@@ -31,8 +32,10 @@ def remove_expired(cache, expire_time):
     spams = [json.loads(n.decode()) for n in redis_client.lrange(cache, 0, -1)]
     expired_count = 0
     for i, n in enumerate(spams):
+        if not n.get('time'):
+            redis_client.lset(cache, i, '__deleted__')
         if time.time() - int(n.get('time', 0)) >= expire_time:
             expired_count += 1
-            redis_client.lset('5_min', i, '__deleted__')
+            redis_client.lset(cache, i, '__deleted__')
     print(f'REMOVED {cache}: {expired_count}')
-    redis_client.lrem('5_min', expired_count, '__deleted__')
+    redis_client.lrem(cache, expired_count, '__deleted__')
